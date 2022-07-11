81006bb5829f6431276772a6ec7f104e03529199
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-11 20:36:09 +0100

    add faucet.sol

--- faucet.sol
+++ faucet.sol
@@ -2,6 +2,10 @@
 
 contract Faucet {
 	recieve () external payable {
-				
+		function withdraw(uint withdraw_amount)	public {
+				require(withdraw_amount <= 100000000000000000);
+				msg.sender.transfer(withdraw_amount);
+			}
+		}	
 	}
 }
